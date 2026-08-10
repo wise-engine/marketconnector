5b53e3e587dbb1406e8dfc4cae38f97ea7682c80
Sunny Srivastav <sunny.me20@gmail.com>
2026-08-10 22:12:53 +0530

    FIX: removed db  folder

--- brokers/database/sqlite.go
+++
@@ -1,8 +0,0 @@
-package database
-
-type SqliteRepo struct{}
-
-func (p *SqliteRepo) GetMasterSymbolId()   {}
-func (p *SqliteRepo) GetMasterSymbolName() {}
-func (p *SqliteRepo) GetBrokerSymbolId()   {}
-func (p *SqliteRepo) GetBrokerSymbolName() {}
